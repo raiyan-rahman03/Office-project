aa959da0c45bde693e8ce8f071c56655dee93855
Raiyan-Rahman-03 <rakiyasultana0172@gmail.com>
2024-03-26 00:07:12 +0600

    models done

--- teebay_project/teebay_backend/models.py
+++ teebay_project/teebay_backend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rent_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    BUY = 'BUY'
+    RENT = 'RENT'
+    TRANSACTION_TYPES = [
+        (BUY, 'Buy'),
+        (RENT, 'Rent'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=4, choices=TRANSACTION_TYPES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name} - {self.transaction_type}"
+
